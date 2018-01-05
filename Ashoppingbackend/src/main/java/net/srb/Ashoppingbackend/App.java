0ecbead9c411e9399eba0c665cb5b73c9dd13f1f
srinivasjava27 <srinivasjava04@gmail.com>
2018-01-05 15:45:23 -0600

    my project setup completed successfully.

---
+++ Ashoppingbackend/src/main/java/net/srb/Ashoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.srb.Ashoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
